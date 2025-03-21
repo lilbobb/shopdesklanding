c0570d58a4ba6ff812c2ce598851db08fe16f31c
Robert Adoga <ukurowo@gmail.com>
2025-03-21 18:31:41 +0100

    fix: fix deployment bug

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: ["your-domain.com", "another-domain.com"],
-    disableStaticImages: false,
-    formats: ["image/webp"],
-  
-    minimumCacheTTL: 60,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
